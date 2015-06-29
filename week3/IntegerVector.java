622a247571a3aa6c7aff59422c68ab1b96b1c53c
anryangelov <a.angelov83y@gmail.com>
2015-06-29 16:24:33 +0300

    Add

--- week3/IntegerVector.java
+++ week3/IntegerVector.java
@@ -1,25 +1,25 @@
 package com.hackbulgaria.programming51.week3;
 
 public class IntegerVector {
-	
-	private int [] data;
+
+	private int[] data;
 	private int capasity;
 	private int size;
-	
+
 	public IntegerVector() {
 		capasity = 10;
-		data = new int [capasity];
-		size = 0;			
+		data = new int[capasity];
+		size = 0;
 	}
-	
+
 	public String toString() {
 		String res = "";
-		for (int i=0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			res += data[i] + " ";
 		}
 		return res;
 	}
-	
+
 	public void add(int e) {
 		if (size >= capasity) {
 			resize();
@@ -27,53 +27,68 @@ public class IntegerVector {
 		data[size] = e;
 		size++;
 	}
-	
+
 	private void resize() {
 		capasity = capasity * 2 + 1;
-		int [] temp = new int [capasity]; 
-		for (int i=0; i < size; i++) {
+		int[] temp = new int[capasity];
+		for (int i = 0; i < size; i++) {
 			temp[i] = data[i];
 		}
-		data = temp;				
+		data = temp;
 	}
-	
+
 	public int get(int index) {
 		return data[index];
 	}
-	
+
 	public int size() {
 		return size;
 	}
-	
+
 	public int sum() {
 		int res = 0;
-		for (int i=0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			res += data[i];
 		}
 		return res;
 	}
-	
-	public void addAll(int [] n) {
-		for (int i:n) {
+
+	public void addAll(int[] n) {
+		for (int i : n) {
 			this.add(i);
 		}
 	}
-
 	
-	public static void main(String [] args) {
+	public void insert(int index, int data) {
+		this.add(0); // some value just to extend the massive;
+		for (int i = size - 1; i > index; i--) {
+			this.data[i] = this.data[i-1];			
+		}
+		this.data[index] = data; 
+	}
+	
+	public void replace(int index1, int index2) {
+		int temp = data[index1];
+		data[index1] = data[index2];
+		data[index2] = temp;
+	}
+
+	public static void main(String[] args) {
 		IntegerVector data = new IntegerVector();
-		int len = 20;
+		int len = 9;
 		for (int i = 0; i < len; i++) {
 			data.add(i);
-		}		
+		}
 		System.out.println(data);
 		System.out.println("size: " + data.size());
 		System.out.println("sum: " + data.sum());
-		int [] n = new int [24];
+		int[] n = new int[2];
 		for (int i = 0; i < n.length; i++) {
 			n[i] = i;
 		}
 		data.addAll(n);
+		System.out.println(data);
+		data.insert(10, 100);
 		System.out.println(data);
 	}
 }
---
+++ week3/IntegerVectorStatic.java
@@ -0,0 +1,78 @@
+package com.hackbulgaria.programming51.week3;
+
+public class IntegerVectorStatic {
+	
+	public static String toString(IntegerVector v) {
+		String res = "";
+		if (v.size() > 0) {
+			for (int i = 0; i < v.size() - 1; i++) {
+				res += v.get(i) + ", ";
+			}
+			res += v.get(v.size() - 1);
+		}
+		return res;
+	}
+	
+	public static void sort(IntegerVector v) {
+		for (int i = 0; i < v.size(); i++) {
+			int temp = i;
+			for (int j = i; j < v.size(); j++) {
+				if (v.get(j) < v.get(temp)) {
+					temp = j;
+				}
+			}
+			v.replace(i, temp);
+		}
+	}
+	
+	public static IntegerVector reverse(IntegerVector v) {
+		IntegerVector res = new IntegerVector();
+		for (int i = 0; i < v.size(); i++) {
+			res.insert(0, v.get(i)); 
+		}
+		return res;
+	}
+	
+	public static String join(IntegerVector v, String glue) {
+		String res = "";
+		if (v.size() > 0) {
+			for (int i = 0; i < v.size() - 1; i++) {
+				res += v.get(i) + glue;
+			}
+			res += v.get(v.size() - 1);
+		}
+		return res;
+	}
+	
+	public static IntegerVector range(int a, int b) {
+		IntegerVector res = new IntegerVector();
+		for (int i = a; i < b; i++) {
+			res.add(i);
+		}
+		return res;
+	}
+
+	public static IntegerVector filterOdd(IntegerVector v) {
+		IntegerVector res = new IntegerVector();
+		return res;
+	}
+	
+	
+	public static void main(String[] args) {
+		IntegerVector data = new IntegerVector();
+		data.add(100);
+		int len = 3;
+		for (int i = 0; i < len; i++) {
+			data.add(i);
+		}
+		data.add(-2);
+		String res = IntegerVectorStatic.toString(data);
+		System.out.println(res);
+		IntegerVectorStatic.sort(data);
+		System.out.println(IntegerVectorStatic.toString(data));
+		IntegerVector r = IntegerVectorStatic.reverse(data);
+		System.out.println(IntegerVectorStatic.toString(r));
+		System.out.println(IntegerVectorStatic.join(data, "->"));
+		System.out.println(IntegerVectorStatic.range(10, 20));
+	}
+}
