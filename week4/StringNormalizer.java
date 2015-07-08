0bced45b88e18aa03ac9cc330891a53377bb38f6
anryangelov <a.angelov83y@gmail.com>
2015-07-08 10:03:33 +0300

    arena does NOT accept String method join

--- week4/StringNormalizer.java
+++ week4/StringNormalizer.java
@@ -8,14 +8,27 @@ public class StringNormalizer {
 	public static String normalizeString(String str) {
 		// TODO: implementation here
 		Vector<String> res = new Vector<String>();
-		String [] words = str.split("\\s+");
-		for (String word: words) {
+		String[] words = str.split("\\s+");
+		for (String word : words) {
 			if (word.length() != 0) {
-				String s = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+				String s = word.substring(0, 1).toUpperCase()
+						+ word.substring(1).toLowerCase();
 				res.add(s);
 			}
 		}
-		return String.join(" ", res);
+		// return String.join(" ", res); // arena does NOT accept join method
+		return join(" ", res);
+	}
+
+	private static String join(String glue, Vector<String> r) {
+		String res = "";
+		if (r.size() > 0) {
+			for (int i = 0; i < r.size() - 1; i++) {
+				res += r.get(i) + glue;
+			}
+			res += r.get(r.size() - 1);
+		}
+		return res;
 	}
 
 	public static void main(String[] args) {
