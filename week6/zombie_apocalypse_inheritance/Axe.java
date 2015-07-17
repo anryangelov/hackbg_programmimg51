e12d9e5f2eee19f030f5773e73c68cf5f966b22f
anryangelov <a.angelov83y@gmail.com>
2015-07-17 10:49:42 +0300

    add constructor

--- week6/zombie_apocalypse_inheritance/Axe.java
+++ week6/zombie_apocalypse_inheritance/Axe.java
@@ -2,7 +2,7 @@ package com.hackbulgaria.programming51.week6.zombie_apocalypse_inheritance;
 
 public class Axe extends ToSmash {
 
-	{
+	public Axe () {
 		durability = currentDurability = 50;
 		damage = 10;
 	}
